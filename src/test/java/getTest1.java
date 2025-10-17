1f6c80dc1bdf39d6764b6b71b9776721280d2492
Ilyas <inurtazin@inbox.ru>
2025-10-17 09:08:41 +0300

    get and response 1

---
+++ src/test/java/getTest1.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class getTest1 {
+    @Test
+    public void testGetTest(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
